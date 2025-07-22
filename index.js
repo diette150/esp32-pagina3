9d56b502eb5e30d2aac1a23cc0986a2def40ac3d
diette <diettedos@gmail.com>
2025-07-22 21:10:46 +0200

    primer commit esp32

---
+++ index.js
@@ -0,0 +1,23 @@
+const express= require('express');
+const cors= require('cors');
+const app= express();
+const PORT= process.env.PORT || 3000
+
+app.use(cors());
+app.use(express.json());
+
+let ultimoDato= {};
+
+app.post('/datos',(req, res)=>{
+    ultimoDato= req.body;
+    console.log('Datos recibidos:', ultimoDato);
+    res.sendStatus(200);
+});
+
+app.get('/datos', (req, res)=>{
+    res.json(ultimoDato);
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+});
